c5134adae3ccd4b3118fbe5184a408631a3143a5
Pranjal Gain <pranjalgain052@gmail.com>
2024-07-15 03:19:18 +0530

    subscribe route changed to subscribes. ToggleSubscription and getUserChannelSubscribers controllers  added.

--- src/controllers/subscribe.controller.js
+++ src/controllers/subscribe.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const Subscription = require("../models/subscription.model");
 const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
@@ -66,21 +67,112 @@ const unsubscribeChannel = asyncHandler(async (req, res) => {
 });
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+    //* Get channel id from req params attribute
+    const { channelId } = req.params;
+
+    //* Sanity check
+    if (channelId === undefined || !channelId)
+        throw new ApiError(400, "No channel found.");
+    try {
+        new mongoose.Types.ObjectId(channelId);
+    } catch (error) {
+        throw new ApiError(400, "Invalid channel.");
+    }
+
+    //* Check if user is subscribed
+    const isSubscribed = await Subscription.findOne({
+        channel: channelId,
+        subscriber: req.user._id,
+    });
+
+    //* If subscribed then unsubscribe else subscribe
+    if (isSubscribed) {
+        const result = await Subscription.findByIdAndDelete(isSubscribed._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, result, "Unubscribed successfully."));
+    } else {
+        const result = await Subscription.create({
+            channel: channelId,
+            subscriber: req.user?._id,
+        });
+        return res
+            .status(201)
+            .json(new ApiResponse(201, result, "Subscribed successfully."));
+    }
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-})
+    const { channelId } = req.params;
+
+    //* Sanity check
+    if (channelId === undefined || !channelId)
+        throw new ApiError(400, "No channel found.");
+    try {
+        new mongoose.Types.ObjectId(channelId);
+    } catch (error) {
+        throw new ApiError(400, "Invalid channel.");
+    }
+
+    const subscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscribers",
+                pipeline:[
+                    {
+                        $project:{
+                            userName:1,
+                            fullName:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            },
+        },
+        {
+            $addFields:{
+                subscriberCount: {
+                    $size:"$subscribers"
+                }
+            }
+        },
+        {
+            $project: {
+                subscribers: 1,
+                subscriberCount:1,
+                _id:0
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscribers[0],
+                "Subscribers fetched successfully."
+            )
+        );
+});
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
-})
+    const { subscriberId } = req.params;
+});
 
 module.exports = {
     subscribeChannel,
     unsubscribeChannel,
+    toggleSubscription,
+    getUserChannelSubscribers
 };
--- src/routes/subscribe.routes.js
+++ src/routes/subscribe.routes.js
@@ -3,12 +3,17 @@ const verifyJwt = require("../middlewares/auth.middleware");
 const {
     subscribeChannel,
     unsubscribeChannel,
+    toggleSubscription,
+    getUserChannelSubscribers,
 } = require("../controllers/subscribe.controller");
 
 const router = express.Router();
 
 router
-    .post("/add", verifyJwt, subscribeChannel)
-    .post("/delete", verifyJwt, unsubscribeChannel);
+    .use(verifyJwt)
+    .post("/add", subscribeChannel)
+    .post("/delete", unsubscribeChannel)
+    .post("/subscribe/:channelId", toggleSubscription)
+    .get("/subscribers/:channelId", getUserChannelSubscribers);
 
-module.exports = router
+module.exports = router;
